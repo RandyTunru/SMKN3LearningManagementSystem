feba424ab1252d3ea0b3b17f12bc79d4de738a73
RandyTunru <randytunru@gmail.com>
2024-05-22 10:43:48 +0800

    feat: added MainLayout

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,14 +1,19 @@
 import React from 'react'; 
-import Sidebar from '../components/Sidebar/Sidebar';
 import Header from '../components/Header/Header';
+import Sidebar from '../components/Sidebar/Sidebar';
+import './MainLayout.css';
 
 export const MainLayout = ({children}) =>{
     return (
-        <div>
-            <Header/>
-            <Sidebar/>
-            <div>
-                {children}
+        <div className=''>
+            <Header Auth="Admin" dashboardPage="/dashboard"/>
+            <div className="sidebar-and-content">
+                <Sidebar />
+                <div className="layout-content">
+                    <div className='content-card'>
+                        {children}
+                    </div>
+                </div>
             </div>
         </div>
     )
